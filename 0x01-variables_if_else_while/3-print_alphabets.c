e2616c883bee987400a9e6d06c00292dd6e1e7b1
root <BankoleJoseph12345>
2022-03-14 13:16:21 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	char c;
+
+	for (a = 'a'; a <= 'z'; ++a)
+	{
+		putchar(a);
+	}
+	for (c = 'A'; c <= 'Z'; ++c)
+	{
+		purchar(c);
+	}
+	purchar('\n');
+	return (0);
+}
