496db871a921c3d3712c0776bbd492290547dfba
root <BankoleJoseph12345>
2022-03-17 01:33:47 -0700

    My alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,13 @@
  * Return: Always 0.
  */
 
-void print_alphabet(void)
+void print_alphabet()
 {
 char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-_putchar(alphabet);
+_putchar('alphabet');
 }
 
 _putchar('\n');
