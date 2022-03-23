707ca877d4a13c2b2b05e38dd43b20181606479a
root <BankoleJoseph12345>
2022-03-23 14:42:01 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,12 +2,12 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table(int n)
+ * @n: number of the times table
  */
-void print_times_table(int n);
-
+void print_times_table(int n)
+{
 	int i, j, k;
-	
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
@@ -18,7 +18,7 @@ void print_times_table(int n);
 				if (j == 0)
 				{
 					_putchar(k + '0');
-				} else if (k < 10 && j !=0)
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -44,4 +44,4 @@ void print_times_table(int n);
 			_putchar('\n');
 		}
 	}
-
+}
