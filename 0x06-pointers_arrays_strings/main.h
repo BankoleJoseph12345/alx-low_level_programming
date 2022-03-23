44bdcc25d4976c7ded89b3bf0ea75ec9b89c992c
root <BankoleJoseph12345>
2022-03-23 14:07:20 -0700

    My main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,20 +1,12 @@
-#include "main.h"
-/**
-* _isupper - evaluate if letter is uppercase.
-*@c: print int
-* Return: Always 0.
-*/
-
-int _isupper(int c)
-{
-if (c >= 'A' &7 c <= 'Z')
-{
-return (1)
-}
-
-else
-{
-return (0);
-}
-
-}
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int _putchar(char c);
