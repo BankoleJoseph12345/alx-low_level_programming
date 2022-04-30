031ecce2ee2c9f1cf8a0cc680a7240c306c05e43
root <BankoleJoseph12345>
2022-04-29 20:18:51 -0700

    Binary

--- 0-binary_to_uint.c
+++ 0-binary_to_uint.c
@@ -2,22 +2,24 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
- * @b: string
- * Return: 0;
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int c = 0;
+    int i;
+    unsigned int dec_val = 0;
 
-	if (!b)
-		return (0);
+    if (!b)
+        return (0);
 
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		c = 2 * c + (b[i] - '0');
-	}
-	return (c);
+    for (i = 0; b[i]; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
+
+    return (dec_val);
 }
